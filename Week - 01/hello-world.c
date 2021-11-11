3096174d0573139b756768b53aa2b912a1115345
ArnobMahmud <arnob.tech.me@gmail.com>
2021-11-11 17:47:09 +0600

    init

---
+++ Week - 01/hello-world.c
@@ -0,0 +1,14 @@
+/*
+    Author : Arnob Mahmud
+
+    mail : arnob.tech.me@gmail.com
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello world!\n");
+    return 0;
+}
